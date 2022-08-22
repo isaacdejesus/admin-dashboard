097d8fd8d97cbe63a934ff82e64d3e5b45a77467
isaac <idreyes@uh.edu>
2022-08-21 19:51:52 -0500

    finished and deployed

--- admin-dashboard/src/components/Navbar.tsx
+++ admin-dashboard/src/components/Navbar.tsx
@@ -86,7 +86,7 @@ const Navbar = () => {
                         />
                         <p>
                             <span className="text-gray-400 text-14">Hi, </span> {' '}
-                            <span className="text-gray-400 font-bold ml-1 text-14">Mic</span>
+                            <span className="text-gray-400 font-bold ml-1 text-14">Joe</span>
                         </p>
                         <MdKeyboardArrowDown className="text-gray-400 text-14"/>
                     </div>
